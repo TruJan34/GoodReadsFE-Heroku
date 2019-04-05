6c07c919f0a2f62a08ba38d637f9eac4b1460725
Trushart Jangade (BSL) <trushartj@kpit.com>
2019-04-05 11:33:16 +0530

    Updates

---
+++ precache-manifest.29f022b94d74446667681d91e0042552.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8178f7cc4b1597b56bbf",
+    "url": "/GoodReadsFE-Heroku/static/js/runtime~main.0fdb599f.js"
+  },
+  {
+    "revision": "772a97d0212bbc4aeb7b",
+    "url": "/GoodReadsFE-Heroku/static/js/main.c53d8820.chunk.js"
+  },
+  {
+    "revision": "10486dbdcf7d15c93e7d",
+    "url": "/GoodReadsFE-Heroku/static/js/2.c6a1c563.chunk.js"
+  },
+  {
+    "revision": "772a97d0212bbc4aeb7b",
+    "url": "/GoodReadsFE-Heroku/static/css/main.4b7e5d7c.chunk.css"
+  },
+  {
+    "revision": "ef20595f326bc69c05b7ca4b55d78de2",
+    "url": "/GoodReadsFE-Heroku/index.html"
+  }
+];
